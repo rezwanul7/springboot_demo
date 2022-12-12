3226a408243a18ac1bb84dc6eaad8aa293c5c7c6
Rezwanul Islam <rezwanul7@gmail.com>
2022-12-12 19:13:30 +0600

    initial commit

---
+++ src/main/java/com/rezwanul7/demo/SpringAppApplication.java
@@ -0,0 +1,13 @@
+package com.rezwanul7.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/rezwanul7/demo/SpringAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rezwanul7.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
